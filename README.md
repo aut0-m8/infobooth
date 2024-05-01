8efca80ea82b621ccd309060989931a66655670e
1 <[email protected]>
2024-04-30 21:43:02 -0500

    added default descriptor and changed code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>kiosk</h1>
+# kiosk
 
 add videos `[indexed].mp4` in common/
 
@@ -6,8 +6,29 @@ add pdfs `[indexed].pdf` in common/
 
 add catalog of videos/pdfs in `catalog.txt`
 
-add splash text in `splash.txt`, compatible with printf formatting
+add splash text in `splash.txt`, compatible with `printf`/`echo -e` formatting
 
 install mpv & mupdf
 
 designed to work on bash and ksh
+
+
+## by default:
+
+| format  | action                                 |
+|---------|----------------------------------------|
+| 0       | Play video                             |
+| *0      | Play playlist from index 1 to indicated index & loop |
+| +0      | Execute command                        |
+| -0      | Open PDF                               |
+
+
+## commands:
+
+| command | action                                    |
+|---------|-------------------------------------------|
+| +0      | Clear screen                              |
+| +1      | Read the catalog/info                     |
+| +2      | Show mpv keybinds                         |
+| +3      | Reboot (password required)                |
+| +4      | Play all videos in a random order infinitely |
--- main.c
+++ main.c
@@ -3,39 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-void play_normal(char *input) {
-  char command[59];
-  sprintf(command, "mpv common/%s.mp4 --loop-file=inf --msg-level=all=no --fs", input);
+void play_normal(char *input, char *args) {
+  char command[100];
+  sprintf(command, "mpv common/%s.mp4 --loop --msg-level=all=no --fs %s", input, args);
   system(command);
 }
 
-void create_playlist(const char *vidIndex) {
+void create_playlist(const char *vidIndex, char *args) {
   FILE *m3uFile = fopen("common/playlist.m3u", "w");
   if (m3uFile != NULL) {
     int index = atoi(vidIndex);
     for (int i = 1; i <= index; i++) {
       fprintf(m3uFile, "%d.mp4\n", i);
     }
-    fclose(m3uFile);
-    system("mpv --playlist=common/playlist.m3u --loop-playlist=inf --msg-level=all=no --fs");
+    if (fclose(m3uFile) != 0) {
+      printf("ERR_M3U_SAVE");
+    } else {
+      char command[100];
+      sprintf(command, "mpv common/playlist.m3u --loop-playlist --msg-level=all=no --fs %s", args);
+      system(command);
+    }
   } else {
     printf("ERR_M3U_NULL\n");
   }
 }
 
-void run_command(const char *input) {
+void run_command(const char *input, char *args) {
   switch (input[0]) {
   case '0':
-    system("clear\n");
+    system("clear");
     break;
   case '1':
-    system("cat catalog.txt\n");
+    system("cat catalog.txt");
+    printf("\n");
     break;
   case '2':
-    system("cat ~/.config/mpv/input.conf\n");
+    system("cat ~/.config/mpv/input.conf");
+    printf("\n");
     break;
   case '3':
     system("sudo reboot"); // adjust based on init system
+    break;
+  case '4':
+    create_playlist("99", "--shuffle");
     break;
   default:
     printf("ERR_ILLEGAL_COMMAND\n");
@@ -44,21 +54,22 @@ void run_command(const char *input) {
 }
 
 void open_pdf(const char *input) {
-  char command[31];
+  char command[50];
   sprintf(command, "mupdf -z 100 -f common/%s.pdf", input);
   system(command);
 }
 
 void filter(char *input) {
+  char args[1] = "\0"; // args still must be passed; set args to null initially
   if (isdigit(input[0])) {
-    play_normal(input);
+    play_normal(input, args);
   } else {
     switch (input[0]) {
     case '*':
-      create_playlist(input + 1);
+      create_playlist(input + 1, args);
       break;
     case '+':
-      run_command(input + 1);
+      run_command(input + 1, args);
       break;
     case '-':
       open_pdf(input + 1);
@@ -72,7 +83,7 @@ void filter(char *input) {
 
 int main() {
   char input[5];
-  system("printf \"%s\" \"$(cat splash.txt)\"");
+  system("echo \"$(cat splash.txt)\""); // add -e argument for escape codes on ksh/openbsd
   while (1) {
     printf("> ");
     scanf("%4s", input);
